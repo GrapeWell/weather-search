9a3a79c467c5c83d29cbffedeb65f9bcf66e2771
zhouhao1 <zhouhao1@cyg.com>
2024-09-29 17:47:30 +0800

    feat: 引入windicss & 一些细节优化

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -1,24 +1,24 @@
-import "./index.less";
+import { Props } from "./types";
 
-const WeatherCard = (props) => {
+const WeatherCard = (props: Props) => {
   const { cityInfo, weatherInfo } = props;
   return (
-    <div className="weather-card">
-      <div className="city-info">
+    <div className="text-white w-500px p-12px border border-solid border-weatherCardBorder rounded bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <div className="flex items-center justify-between">
         <div>
           <span style={{ marginRight: "10px" }}>{cityInfo.city}</span>
           <span style={{ marginRight: "10px" }}>{cityInfo.district}</span>
           {cityInfo.district !== cityInfo.name && <span>{cityInfo.name}</span>}
         </div>
       </div>
-      <div className="weather-info">
-        <div className="temperature">
-          <span className="temp">{weatherInfo.temp}</span>
+      <div className="mt-12px flex items-center justify-between">
+        <div>
+          <span className="text-[24px]">{weatherInfo.temp}</span>
           <span>℃</span>
         </div>
         <div>{weatherInfo.text}</div>
       </div>
-      <div className="detail-info">
+      <div className="flex items-center justify-between mt-[12px]">
         <div className="feels-like">体感温度：{weatherInfo.feelsLike}℃</div>
         <div>风速：{weatherInfo.windSpeed}公里/小时</div>
         <div>湿度：{weatherInfo.humidity}%</div>
---
+++ windi.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+    theme: {
+      extend: {
+        colors: {
+          homeBackground: "#333333",
+          weatherCardBorder: '#cccccc',
+        },
+      },
+    },
+  })
