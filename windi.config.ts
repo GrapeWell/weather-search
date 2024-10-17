f425c3d8963b2eaa567e2cb54095e1e8b9957105
zhouhao1 <zhouhao1@cyg.com>
2024-10-17 16:20:48 +0800

    feat: 修改网站名称和logo & 修改24小时天气为卡片样式

--- windi.config.ts
+++ windi.config.ts
@@ -3,12 +3,20 @@ import { defineConfig } from 'windicss/helpers';
 export default defineConfig({
   theme: {
     extend: {
+      backgroundColor: {
+        base: 'var(--bg-base)',
+        primary: 'var(--bg-primary)',
+        secondary: 'var(--bg-secondary)',
+      },
+      textColor: {
+        base: 'var(--text-base)',
+        primary: 'var(--text-primary)',
+        secondary: 'var(--text-secondary)',
+      },
       colors: {
         homeBackground: '#F5F5F5',
-        weatherCardBorder: '#cccccc',
         textPrimary: '#4A4A4A',
         textSecondary: '#A0A0A0',
-        borderPrimary: 'E0E0E0',
       },
     },
   },
