4bc2cd1380a18f567ad3fed22ee45522a9c808e1
zhouhao1 <zhouhao1@cyg.com>
2025-01-08 15:26:52 +0800

    fix: 将请求代理到服务器

--- src/pages/Home/services.ts
+++ src/pages/Home/services.ts
@@ -1,41 +1,31 @@
-import axios from 'axios';
+import { default as axios } from '@/request';
 
 // 获取城市信息
 export const getCityInfo = async (latitud: number, longitud: number) => {
   return await axios.get(
-    `https://geoapi.qweather.com/v2/city/lookup?location=${longitud.toFixed(2)},${latitud.toFixed(2)}&key=cdc2853484e341e0a1efb073503129a4`,
+    `/api/city/lookup?location=${longitud.toFixed(2)},${latitud.toFixed(2)}`,
   );
 };
 
 export const getCityInfoByCityName = async (cityName: string) => {
-  return await axios.get(
-    `https://geoapi.qweather.com/v2/city/lookup?location=${cityName}&key=cdc2853484e341e0a1efb073503129a4`,
-  );
+  return await axios.get(`/api/city/lookup?location=${cityName}`);
 };
 
 // 获取当前天气
 export const getWeatherInfo = async (cityId: string) => {
-  return await axios.get(
-    `https://devapi.qweather.com/v7/weather/now?location=${cityId}&key=cdc2853484e341e0a1efb073503129a4`,
-  );
+  return await axios.get(`/api/weather/now?location=${cityId}`);
 };
 
 // 获取7天预报
 export const get7DayForecast = async (locationId: string) => {
-  return await axios.get(
-    `https://devapi.qweather.com/v7/weather/7d?location=${locationId}&key=cdc2853484e341e0a1efb073503129a4`,
-  );
+  return await axios.get(`/api/weather/7d?location=${locationId}`);
 };
 
 // 获取24小时预报
 export const get24HoursForecast = async (locationId: string) => {
-  return await axios.get(
-    `https://devapi.qweather.com/v7/weather/24h?location=${locationId}&key=cdc2853484e341e0a1efb073503129a4`,
-  );
+  return await axios.get(`/api/weather/24h?location=${locationId}`);
 };
 
 export const getLifeIndex = async (locationId: string) => {
-  return await axios.get(
-    `https://devapi.qweather.com/v7/indices/1d?location=${locationId}&key=cdc2853484e341e0a1efb073503129a4&type=1,2,3,5`,
-  );
+  return await axios.get(`/api/indices/1d?location=${locationId}&type=1,2,3,5`);
 };
--- src/request.ts
+++ src/request.ts
@@ -1,3 +1,17 @@
 import axios from 'axios';
 
-const createService = axios.create({});
+const axiosInstance = axios.create({});
+
+// 添加响应拦截器
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // 自动返回 response.data
+    return response.data;
+  },
+  (error) => {
+    // 在请求失败时抛出错误
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
