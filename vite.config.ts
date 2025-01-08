070b7ffac5934d7edead40f8a2db1e13b4f064b1
GrapeWell <1016939850@qq.com>
2025-01-08 15:26:52 +0800

    fix: 将请求代理到服务器

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
 import path from 'path';
-import findUnusedFilesPlugin from 'vite-plugin-unused-files';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    WindiCSS(),
-    findUnusedFilesPlugin({
-      entryFile: 'src/main.tsx', // 设置入口文件
-      alias: { '@': 'src' }, // 别名配置
-      include: ['src/**/*.{tsx,ts,jsx,js,css,less,png,jpg,gif,svg}'],
-      exclude: ['src/**/*.d.ts'],
-      dryRun: true, // 先运行 dryRun 确认未使用文件
-    }),
-  ],
+  plugins: [react(), WindiCSS()],
   css: {
     preprocessorOptions: {
       less: {
@@ -27,6 +16,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://129.28.95.21',
+        changeOrigin: true,
+      },
     },
   },
 });
